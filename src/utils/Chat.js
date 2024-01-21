bf84933d48f29c1af6c2955a4c5ac95f44e7a87c
Sepehr-A <sepehr.alidousti@gmail.com>
2024-01-21 17:14:10 +0100

    removing unused css

--- src/utils/Chat.js
+++ src/utils/Chat.js
@@ -17,30 +17,36 @@ const Chat = ({messages, message, setMessage, handleKeyDown, sendMessage}) => {
         scrollToBottom(); // Call this function whenever messages change
     }, [messages]);
     return (
-        <div className="chat-container">
-            <div className="message-list">
-                {messages.map((msg, index) => (
-                    <div
-                        key={index}
-                        className={`message-item ${msg.role === 'server' ? 'server-message' : 'client-message'}`}>
-                        <div>
-                            <div className="message-text">{msg.text}</div>
-                            <div className="message-timestamp">{formatTimestamp(msg.timestamp)}</div>
+        <div className="app-container">
+            <div className="app-wrapper">
+                <div className="content-container">
+                    <div className="chat-container">
+                        <div className="message-list">
+                            {messages.map((msg, index) => (
+                                <div
+                                    key={index}
+                                    className={`message-item ${msg.role === 'server' ? 'server-message' : 'client-message'}`}>
+                                    <div>
+                                        <div className="message-text">{msg.text}</div>
+                                        <div className="message-timestamp">{formatTimestamp(msg.timestamp)}</div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                        <div className="input-container">
+                            <input
+                                className="message-input"
+                                value={message}
+                                onChange={(e) => setMessage(e.target.value)}
+                                onKeyDown={handleKeyDown}
+                                placeholder="Type your message..."
+                            />
+                            <button className="send-button" onClick={() => sendMessage(message)}>
+                                Send
+                            </button>
                         </div>
                     </div>
-                ))}
-            </div>
-            <div className="input-container">
-                <input
-                    className="message-input"
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                    onKeyDown={handleKeyDown}
-                    placeholder="Type your message..."
-                />
-                <button className="send-button" onClick={() => sendMessage(message)}>
-                    Send
-                </button>
+                </div>
             </div>
         </div>
     );
